acaead00881f098723d3b5159f4070b1ac86bb2f
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2022-11-07 08:40:01 +0000

    preview for video element to upload.

--- static/upload.js
+++ static/upload.js
@@ -4,7 +4,6 @@
 let counter = 0;
 
 const removeRow = target => ev => {
-// function removeRow(ev) {
   console.log("clicked", ev);
   target.parentNode.removeChild(target);
 }
@@ -12,20 +11,53 @@ const removeRow = target => ev => {
 const loadImagePreview = inputElement => {
   let imgEl = document.createElement("img");
   imgEl.src = "//:0";
-
+  imgEl.style.display = "none";
   inputElement.insertAdjacentElement("afterend", imgEl);
+
+  let vidEl = document.createElement("video");
+  vidEl.controls = true;
+  vidEl.width = "500";
+  vidEl.height = "400";
+  vidEl.style.display = "none";
+  inputElement.insertAdjacentElement("afterend", vidEl);
+  window.vidEl = vidEl;
+
   inputElement.addEventListener("change", (_ev) => {
     if (inputElement.files && inputElement.files[0]) {
-      if ( inputElement.files[0].type.startsWith("image")) {
+      let file = inputElement.files[0];
+      console.log(`mimetype is: ${file.type}`);
+      window.f = inputElement.files[0];
+      if (file.type.startsWith("image")) {
         imgEl.alt = `preview for ${inputElement.name}`;
-        imgEl.src = URL.createObjectURL(inputElement.files[0]);
+        imgEl.src = URL.createObjectURL(file);
+        imgEl.style.display = "block";
         imgEl.onload = () => {
+          console.log("revoking url for image");
           URL.revokeObjectURL(imgEl.src);
         }
       } else {
         imgEl.src = "//:0";
         imgEl.alt = "";
+        imgEl.style.display = "none";
       }
+
+      if (file.type.startsWith("video")) {
+        let vidSrc = document.createElement("source");
+        vidSrc.source = "//:0";
+        vidEl.insertAdjacentElement("beforeEnd", vidSrc);
+        window.vidSrc = vidSrc;
+
+        vidEl.style.display = "block";
+        let u = URL.createObjectURL(file);
+        vidSrc.src = u;
+        vidSrc.type = file.type;
+        // no onload=>revokeObjectURL since it doesn't seem to work (?)
+      } else {
+        vidEl.removeChild(vidSrc);
+        vidSrc.src = "//:0";
+        vidEl.style.display = "none";
+      }
+
     }
   });
 
@@ -42,7 +74,6 @@ function addFile(id) {
   inputElement.name = `file_${counter}`;
 
   p.insertAdjacentElement("afterbegin", inputElement);
-  // p.insertAdjacentHTML("afterbegin", `<input type="file" name="file_${counter}">`);
   loadImagePreview(inputElement);
   let closeButton = document.createElement("button");
   closeButton.setAttribute("type", "button");
@@ -52,17 +83,26 @@ function addFile(id) {
   return p;
 }
 
-function addRow(_ev) {
+const addRow = (endEl) => (_ev) => {
   counter++;
   let el = addFile(counter);
-  const button = document.querySelector("#upload-form [type=submit]");
-  button.insertAdjacentElement("beforebegin", el);
+  endEl.insertAdjacentElement("beforebegin", el);
 }
 
 window.onload = function onload() {
-  document.getElementById("add-file").addEventListener("click", addRow);
-  console.log("coucou upload");
-  addRow()
-  // const button = document.querySelector("#upload-form [type=submit]");
-  // button.insertAdjacentElement("beforebegin", addFile(1));
+  let p = document.createElement("p");
+  let button = document.createElement("button");
+  button.type = "button";
+  button.innerText = "Add file";
+  p.insertAdjacentElement("afterbegin", button);
+  document.querySelector("#upload-form").insertAdjacentElement("afterbegin", p);
+
+  let p2 = p.cloneNode(true);
+  document.querySelector("#upload-form [type='submit']").insertAdjacentElement("beforebegin", p2);
+
+  let addRowInForm = addRow(p2);
+  p.addEventListener("click", addRowInForm);
+  p2.addEventListener("click", addRowInForm);
+
+  addRowInForm();
 }
